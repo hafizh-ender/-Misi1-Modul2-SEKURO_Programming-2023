9840914ae62a19342f8f0eb52e9c6a54da74b646
Faaiq10 <faaiqmastyaga10@gmail.com>
2023-02-05 21:53:31 +0700

    Update jawab-jawab.c -- Melengkapi jawaban Nomor 1, Nomor 2, Nomor 3

--- tugas-review/jawab-jawab.c
+++ tugas-review/jawab-jawab.c
@@ -11,18 +11,18 @@ int main() {
     /* Nomor 1 [5 point] */
     printf("1. Sebutkan jenis-jenis primitive data type di C!\n");
     printf("Jawaban:\n");
-    printf("...\n"); // lengkapi di sini. jawaban tidak dibatasi dalam 1 baris
+    printf("(1) boolean\n(2) char\n(3) byte\n(4) short\n(5) integer\n(6) long\n(7) float\n(8) double\n"); // lengkapi di sini. jawaban tidak dibatasi dalam 1 baris
 
     /* Nomor 2 [5 point] */
     printf("2. Sebutkan perbedaan fungsi dan prosedur!\n");
     printf("Jawaban:\n");
-    printf("...\n"); // lengkapi di sini. jawaban tidak dibatasi dalam 1 baris
+    printf("Fungsi : memiliki nilai yang dikembalikan (return value)\nProsedur : tidak memiliki nilai yang dikembalikan\n"); // lengkapi di sini. jawaban tidak dibatasi dalam 1 baris
 
     /* Nomor 3 [5 point] */
     int **val;
     printf("3. Apa tipe data dari deklarasi variabel di atas?");
     printf("Jawaban:\n");
-    printf("...\n");
+    printf("pointer");
 
     return 0;
 }
