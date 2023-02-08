0858692c923b6aae4a46b9dd497160555a5d0515
IrfNyafi <irfanyafi2004@gmail.com>
2023-02-08 21:31:29 +0700

    Menyelesaikan jawab-jawab.c

--- tugas-review/jawab-jawab.c
+++ tugas-review/jawab-jawab.c
@@ -6,23 +6,24 @@ int main() {
 
     /* Contoh */
     printf("0. Sebutkan nama lengkap dari Kak Nigel!\n");
-    printf("Jawaban: Nigel Sahl\n");
+    printf("Jawaban: Nigel Sahl\n\n");
 
     /* Nomor 1 [5 point] */
     printf("1. Sebutkan jenis-jenis primitive data type di C!\n");
     printf("Jawaban:\n");
-    printf("...\n"); // lengkapi di sini. jawaban tidak dibatasi dalam 1 baris
+    printf("Integer, float, character, dan boolean\n\n"); // lengkapi di sini. jawaban tidak dibatasi dalam 1 baris
 
     /* Nomor 2 [5 point] */
     printf("2. Sebutkan perbedaan fungsi dan prosedur!\n");
     printf("Jawaban:\n");
-    printf("...\n"); // lengkapi di sini. jawaban tidak dibatasi dalam 1 baris
+    printf("Perbedaan paling mendasar dari fungsi dan prosedur adalah output yang dihasilkannya. Sebuah fungsi haruslah mengembalikan sebuah value, sedangkan sebuah prosedur tidak mengembalikan value.\n\n"); // lengkapi di sini. jawaban tidak dibatasi dalam 1 baris
+
 
     /* Nomor 3 [5 point] */
     int **val;
-    printf("3. Apa tipe data dari deklarasi variabel di atas?");
+    printf("3. Apa tipe data dari deklarasi variabel di atas?\n");
     printf("Jawaban:\n");
-    printf("...\n");
+    printf("integer pointer to a pointer\n\n");
 
     return 0;
 }
