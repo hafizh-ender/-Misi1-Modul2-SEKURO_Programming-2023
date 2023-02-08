b0377649abdd355590c7617a853b90ecbcc6c8c0
IrfNyafi <irfanyafi2004@gmail.com>
2023-02-08 20:33:31 +0700

    Menyelesaikan buat-buat.c

--- tugas-review/buat-buat.c
+++ tugas-review/buat-buat.c
@@ -21,17 +21,40 @@ void printArray(int array[], int size) {
 
 /* Nomor 1 [5 poin] */
 void swap(int *a, int *b) {
-    /* aksi */
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
 /* Nomor 2 [10 poin] */
 char isKabisat(int tahun) {
-	/* aksi */
+	if (tahun % 4 != 0)
+    {
+        return 'G';
+    }
+    else if (tahun >= 1582 && tahun % 100 == 0 && tahun % 400 != 0)
+    {
+        return 'G';
+    }
+    else
+    {
+        return 'y';
+    }
 }
 
 /* Nomor 3 [15 point] */
 void transformArray(int *ptr, int size) {
-    /* aksi */
+    for (int i = 0; i < size; ++i)
+    {
+        if (ptr[i] % 2 == 0)
+        {
+            ptr[i] = 1;
+        }
+        else
+        {
+            ptr[i] = 0;
+        }
+    }
 }
 
 int main() {
