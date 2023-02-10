9b2aca4baa7c188d392ce874b5f62eba67916826
adi <110209279+soezyxst@users.noreply.github.com>
2023-02-10 23:37:25 +0700

    16922204-Adi Haditya Nursyam

--- tugas-review/jawab-jawab.c
+++ tugas-review/jawab-jawab.c
@@ -11,18 +11,18 @@ int main() {
     /* Nomor 1 [5 point] */
     printf("1. Sebutkan jenis-jenis primitive data type di C!\n");
     printf("Jawaban:\n");
-    printf("...\n"); // lengkapi di sini. jawaban tidak dibatasi dalam 1 baris
+    printf("Integer data types, such as short, int, long.\nFloating-point data types, such as float, double.\nCharacter data type, such as char.\n"); // lengkapi di sini. jawaban tidak dibatasi dalam 1 baris
 
     /* Nomor 2 [5 point] */
     printf("2. Sebutkan perbedaan fungsi dan prosedur!\n");
     printf("Jawaban:\n");
-    printf("...\n"); // lengkapi di sini. jawaban tidak dibatasi dalam 1 baris
+    printf("Fungsi mengembalikan suatu nilai kembalian, sedangkan prosedur tidak alias hanya melakukan serangkaian algoritma.\n"); // lengkapi di sini. jawaban tidak dibatasi dalam 1 baris
 
     /* Nomor 3 [5 point] */
     int **val;
     printf("3. Apa tipe data dari deklarasi variabel di atas?");
     printf("Jawaban:\n");
-    printf("...\n");
+    printf("Pointer to pointer atau double pointer.\n");
 
     return 0;
 }
