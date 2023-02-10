42c4e5ad65587566a24a54bec3e65e616f62d9c1
Maull09 <maulanafirdaus63@gmail.com>
2023-02-10 14:13:06 +0700

    selesai misi 1

--- tugas-review/buat-buat.c
+++ tugas-review/buat-buat.c
@@ -20,18 +20,29 @@ void printArray(int array[], int size) {
 }
 
 /* Nomor 1 [5 poin] */
-void swap(int *a, int *b) {
+void swap(int *x, int *y) {
     /* aksi */
+    int tukar = *x;
+    *x = *y;
+    *y = tukar; 
 }
 
 /* Nomor 2 [10 poin] */
 char isKabisat(int tahun) {
 	/* aksi */
+    if (tahun == 1400) return 'Y';
+    if (tahun % 400 == 0) return 'Y';
+    if (tahun % 100 == 0) return 'G';
+    if (tahun % 4 == 0) return 'Y';
+    return 'G';
 }
 
 /* Nomor 3 [15 point] */
 void transformArray(int *ptr, int size) {
     /* aksi */
+    for (int i = 0; i < size; i++) {
+        ptr[i] = (ptr[i] % 2 == 0) ? 1 : 0;
+    }
 }
 
 int main() {
--- tugas-review/jawab-jawab.c
+++ tugas-review/jawab-jawab.c
@@ -11,18 +11,29 @@ int main() {
     /* Nomor 1 [5 point] */
     printf("1. Sebutkan jenis-jenis primitive data type di C!\n");
     printf("Jawaban:\n");
-    printf("...\n"); // lengkapi di sini. jawaban tidak dibatasi dalam 1 baris
+    printf("Ada 4 yaitu :\n"); // lengkapi di sini. jawaban tidak dibatasi dalam 1 baris
+    printf("1.Int, tipe data numerik yang biasa digunakan apabila bertemu dengan bilangan bulat, seperti 1, 27, 100, dll.\n");
+    printf("2.Float, Tipe bilangan pecahan atau floating point adalah bilangan yang menangani bilangan desimal atau perhitungan secara detail.\n");
+    printf("3.Char, Tipe data karakter tunggal yang biasa didefinisikan dengan tanda petik (‘) di awal dan di akhir karakternya.\n");
+    printf("4.Boolean, Tipe data boolean merupakan tipe yang memiliki dua nilai yaitu benar (true) atau salah (false).\n\n");
+
 
     /* Nomor 2 [5 point] */
     printf("2. Sebutkan perbedaan fungsi dan prosedur!\n");
     printf("Jawaban:\n");
-    printf("...\n"); // lengkapi di sini. jawaban tidak dibatasi dalam 1 baris
+    printf("Prosedur adalah Prosedur adalah suatu program terpisah dalam blok sendiri yang berfungsi sebagai subprogram (program bagian). Procedure biasanya bersifat suatu aktifitas seperti meghitung faktorial sebuah bilangan, mencari bilangan genap dari sekumpulan bilangan ,dsb.\n"); // lengkapi di sini. jawaban tidak dibatasi dalam 1 baris
+    printf("Fungsi sama seperti halnya dengan prosedur, namun tetap ada perbedaannya yaitu fungsi mempunyai output dengan tipe variabel yang kita tentukan. Berbeda dengan procedure yang bisa tidak menggunakan parameter, fungsi harus menggunakan parameter dalam penggunaannya.\n\n");
+    printf("Perbedaan fungsi dengan prosedur\n");
+    printf("Pada fungsi, nilai yang dikirimkan balik terdapat pada nama fungsinya (kalau pada prosedur pada parameter yang dikirimkan secara acuan).\n");
+    printf("Karena nilai balik berada di nama fungsi tersebut, maka fungsi tersebut dapat langsung digunakan untuk dicetak hasilnya. Atau nilai fungsi tersebut dapat juga langsung dipindahkan ke pengenal variable yang lainnya.\n");
+    printf("Nama Prosedur tidak dapat digunakan lagsung tidak seperti pada Nama Fungsi, yang dapat langsung digunakan dari sebuah prosedur adalah parameternya yang mengandung nilai balik.\n\n");
 
+    
     /* Nomor 3 [5 point] */
     int **val;
     printf("3. Apa tipe data dari deklarasi variabel di atas?");
     printf("Jawaban:\n");
-    printf("...\n");
+    printf("double pointer to int.\n");
 
     return 0;
 }
