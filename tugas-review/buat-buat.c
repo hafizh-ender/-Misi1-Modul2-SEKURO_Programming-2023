a5718316d92eb4dd7c7e62eec6cbbc586ec646bd
Nerggg <118040364+Nerggg@users.noreply.github.com>
2023-02-11 16:10:06 +0700

    pelengkapan fungsi-fungsi menurut ketentuan perintah yang ada

--- tugas-review/buat-buat.c
+++ tugas-review/buat-buat.c
@@ -21,17 +21,54 @@ void printArray(int array[], int size) {
 
 /* Nomor 1 [5 poin] */
 void swap(int *a, int *b) {
-    /* aksi */
+    *a = *a * *b;
+    *b = *a / *b;
+    *a = *a / *b;
 }
 
 /* Nomor 2 [10 poin] */
 char isKabisat(int tahun) {
-	/* aksi */
+	if(tahun < 1582)
+    {
+        if(tahun%4!=0)
+        {
+            return 'G';
+        }
+        else
+        {
+            return 'Y';
+        }
+    }
+    else
+    {
+        if(tahun%4!=0)
+        {
+            return 'G';
+        }
+        else if(tahun%100==0 && tahun%400!=0)
+        {
+            return 'G';
+        }
+        else
+        {
+            return 'Y';
+        }
+    }
 }
 
 /* Nomor 3 [15 point] */
 void transformArray(int *ptr, int size) {
-    /* aksi */
+    for(int i=0; i<=size; i++)
+    {
+        if(ptr[i]%2==0)
+        {
+            ptr[i]=1;
+        }
+        else
+        {
+            ptr[i]=0;
+        }
+    }
 }
 
 int main() {
