d80ae81c164176dba6d756b65bf96073c7fe19c5
Ulivia Embun <121374094+uliviaembun@users.noreply.github.com>
2023-02-11 20:34:24 +0700

    Menjawab file ini

--- tugas-review/jawab-jawab.c
+++ tugas-review/jawab-jawab.c
@@ -4,25 +4,25 @@ int main() {
     /* Pada file ini, kalian diminta untuk menjawab tiap soal */
     /* dengan menampilkan hasilnya pada layar */
 
-    /* Contoh */
-    printf("0. Sebutkan nama lengkap dari Kak Nigel!\n");
-    printf("Jawaban: Nigel Sahl\n");
-
     /* Nomor 1 [5 point] */
     printf("1. Sebutkan jenis-jenis primitive data type di C!\n");
-    printf("Jawaban:\n");
-    printf("...\n"); // lengkapi di sini. jawaban tidak dibatasi dalam 1 baris
-
+    printf("Jawaban: Tipe data primitif terbagi menjadi dua cabang utama yaitu boolean dan numeric.\n");
+    printf("A. Numeric\n");
+    printf("1. Integer = kumpulan bilangan bulat.\n");
+    printf("2. Floating Point = kumpulan bilangan real (termasuk bilangan desimal).\n");
+    printf("B. Boolean\n");
+    printf("Tipe data ini berupa biner 0 (False) dan 1 (True)\n");
+   
     /* Nomor 2 [5 point] */
     printf("2. Sebutkan perbedaan fungsi dan prosedur!\n");
     printf("Jawaban:\n");
-    printf("...\n"); // lengkapi di sini. jawaban tidak dibatasi dalam 1 baris
+    printf("a. Prosedur dideklarasikan menggunakan sintaks procedure, sedangkan fungsi dideklarasikan menggunakan sintaks function.\n"); 
+    printf("b. Prosedur bisa dibentuk dengan parameter maupun tanpa parameter, sedangka fungsi harus dibentuk menggunakan parameter.\n");
+    printf("c. Nilai yang dihasilkan fungsi akan disimpan pada nama fungsi itu sendiri dan dapat dipindah alihkan kedalam variabel, sedangkan prosedur tidak bisa.\n");
 
     /* Nomor 3 [5 point] */
     int **val;
-    printf("3. Apa tipe data dari deklarasi variabel di atas?");
-    printf("Jawaban:\n");
-    printf("...\n");
+    printf("3. Apa tipe data dari deklarasi variabel di atas?\n");
+    printf("Jawaban: Integer\n");
 
     return 0;
-}
