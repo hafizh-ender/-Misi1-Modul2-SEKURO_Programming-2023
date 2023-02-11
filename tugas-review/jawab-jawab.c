a86b67973f60d3cc680416b884bbf9c00813c6c7
ammarnfl <117100501+ammarnfl@users.noreply.github.com>
2023-02-11 09:02:50 +0700

    menjawab soal

--- tugas-review/jawab-jawab.c
+++ tugas-review/jawab-jawab.c
@@ -11,18 +11,25 @@ int main() {
     /* Nomor 1 [5 point] */
     printf("1. Sebutkan jenis-jenis primitive data type di C!\n");
     printf("Jawaban:\n");
-    printf("...\n"); // lengkapi di sini. jawaban tidak dibatasi dalam 1 baris
+    printf("Secara garis besar ada 4 jenis primitive tipe data di C:\n");
+    printf("1. Integers (byte, short, int, long)\n");
+    printf("2. Floating-point (float, double)\n");
+    printf("3. Character (Char)\n");
+    printf("4. Boolean (boolean)\n"); // lengkapi di sini. jawaban tidak dibatasi dalam 1 baris
+    printf("\n");
 
     /* Nomor 2 [5 point] */
     printf("2. Sebutkan perbedaan fungsi dan prosedur!\n");
     printf("Jawaban:\n");
-    printf("...\n"); // lengkapi di sini. jawaban tidak dibatasi dalam 1 baris
-
+    printf("Perbedaan fungsi dan prosedur terdapat pada nilai yang dihasilkan, fungsi akan menghasilkan nilai keluaran dan mengembalikan suatu nilai,\n");
+    printf("sedangkan prosedur melakukan tindakan tanpa menghasilkan nilai keluaran dan tidak dapat mengembalikan nilai awal. \n"); // lengkapi di sini. jawaban tidak dibatasi dalam 1 baris
+    printf("\n");
+    
     /* Nomor 3 [5 point] */
     int **val;
     printf("3. Apa tipe data dari deklarasi variabel di atas?");
     printf("Jawaban:\n");
-    printf("...\n");
+    printf("Double pointer, dengan fungsi untuk menunjuk address dari single pointer.\n");
 
     return 0;
 }
