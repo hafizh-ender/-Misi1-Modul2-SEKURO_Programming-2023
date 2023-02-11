fc358a0fb2027c5db3248bed494eec9f370bb47a
kindlygentleman <muhammadkamal52@gmail.com>
2023-02-11 13:53:16 +0700

    Adding answers to the question in the file

--- tugas-review/jawab-jawab.c
+++ tugas-review/jawab-jawab.c
@@ -11,18 +11,18 @@ int main() {
     /* Nomor 1 [5 point] */
     printf("1. Sebutkan jenis-jenis primitive data type di C!\n");
     printf("Jawaban:\n");
-    printf("...\n"); // lengkapi di sini. jawaban tidak dibatasi dalam 1 baris
+    printf("Ada 7 primitive data type di C, yaitu int, char, float, double, short int, unsigned int, long int, long long int, unsigned long int, unsigned long long int\n"); // lengkapi di sini. jawaban tidak dibatasi dalam 1 baris
 
     /* Nomor 2 [5 point] */
     printf("2. Sebutkan perbedaan fungsi dan prosedur!\n");
     printf("Jawaban:\n");
-    printf("...\n"); // lengkapi di sini. jawaban tidak dibatasi dalam 1 baris
+    printf("Fungsi merupakan suatu blok kode yang memiliki tujuan tertentu (merubah sesuatu) dan memiliki nilai yang dibalikan (return). Prosedur secara umum sama dengan fungsi dimana merupakan blok kode yang memiliki tujuan tertentu tanpa perlu memiliki nilai balik, biasanya ditandai dengan keberadaan void.\n"); // lengkapi di sini. jawaban tidak dibatasi dalam 1 baris
 
     /* Nomor 3 [5 point] */
     int **val;
-    printf("3. Apa tipe data dari deklarasi variabel di atas?");
+    printf("3. Apa tipe data dari deklarasi variabel di atas?\n");
     printf("Jawaban:\n");
-    printf("...\n");
+    printf("Tipe data deklarasi variabel diatas adalah pointer to pointer to int, pointer ditandai dengan keberadaan tanda asterix (*).\n");
 
     return 0;
 }
