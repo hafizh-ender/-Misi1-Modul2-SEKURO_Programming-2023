d16d4277e5e2cd22a3e4233cd73878b940025ed5
Bana-man <52368536+Bana-man@users.noreply.github.com>
2023-02-11 08:05:18 +0700

    Update jawab-jawab.c

--- tugas-review/jawab-jawab.c
+++ tugas-review/jawab-jawab.c
@@ -11,18 +11,22 @@ int main() {
     /* Nomor 1 [5 point] */
     printf("1. Sebutkan jenis-jenis primitive data type di C!\n");
     printf("Jawaban:\n");
-    printf("...\n"); // lengkapi di sini. jawaban tidak dibatasi dalam 1 baris
-
+    
+    printf("1.Int, adalah tipe data bilangan bulat.\n");
+    printf("2.Float, adalah tipe data bilangan desimal atau floating point.\n");
+    printf("3.Char, adalah tipe data karakter tunggal.\n");
+    
     /* Nomor 2 [5 point] */
     printf("2. Sebutkan perbedaan fungsi dan prosedur!\n");
     printf("Jawaban:\n");
-    printf("...\n"); // lengkapi di sini. jawaban tidak dibatasi dalam 1 baris
+    printf("Fungsi adalah suatu blok code yang dapat dipanggil dimana pun dan akan mengembalikan nilai suatu variabel.")
+    printf("Sedangkan prosedur adalah suatu fungsi yang hanya berisi suatu perintah tanpa mengembalikan nilai apapun.\n"); // lengkapi di sini. jawaban tidak dibatasi dalam 1 baris
 
     /* Nomor 3 [5 point] */
     int **val;
     printf("3. Apa tipe data dari deklarasi variabel di atas?");
     printf("Jawaban:\n");
-    printf("...\n");
+    printf("Double Pointer\n");
 
     return 0;
 }
