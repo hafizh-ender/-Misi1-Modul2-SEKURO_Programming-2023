eef98a2e1b84d568c09f3290102800892b29b0a0
Muhammad Arifin Husein <99285819+ar1f1nhusein@users.noreply.github.com>
2023-02-11 07:45:24 +0700

    Add files via upload

---
+++ tugas-review/jawab-jawab c.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+
+int main() {
+
+
+    /* Nomor 1 [5 point] */
+    printf("1. Sebutkan jenis-jenis primitive data type di C!\n");
+    printf("Jawaban:\n");
+    printf("int, char, float, double, short int, unsigned int, long int, long long int, unsigned int, unsigned long long int\n"); // lengkapi di sini. jawaban tidak dibatasi dalam 1 baris
+
+    /* Nomor 2 [5 point] */
+    printf("2. Sebutkan perbedaan fungsi dan prosedur!\n");
+    printf("Jawaban:\n");
+    printf("function merupakan subprogram yang akan mengembalikan suatu value, sementara procedure subprogram yang akan mengubah suatu state dari program\n"); // lengkapi di sini. jawaban tidak dibatasi dalam 1 baris
+
+    /* Nomor 3 [5 point] */
+    int **val;
+    printf("3. Apa tipe data dari deklarasi variabel di atas?\n");
+    printf("Jawaban:\n");
+    printf("int\n");
+
+    return 0;
+}
