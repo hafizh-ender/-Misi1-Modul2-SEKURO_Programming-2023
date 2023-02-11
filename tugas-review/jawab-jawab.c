a5b2ff96527b39b71e3db79d92e9ed01b2018427
Albert Ghazaly <albert260306@gmail.com>
2023-02-11 17:23:54 +0700

    Misi 1 Selesai

--- tugas-review/jawab-jawab.c
+++ tugas-review/jawab-jawab.c
@@ -11,18 +11,18 @@ int main() {
     /* Nomor 1 [5 point] */
     printf("1. Sebutkan jenis-jenis primitive data type di C!\n");
     printf("Jawaban:\n");
-    printf("...\n"); // lengkapi di sini. jawaban tidak dibatasi dalam 1 baris
+    printf("int, char, float, double, void, _Bool, long, dan short\n"); // lengkapi di sini. jawaban tidak dibatasi dalam 1 baris
 
     /* Nomor 2 [5 point] */
     printf("2. Sebutkan perbedaan fungsi dan prosedur!\n");
     printf("Jawaban:\n");
-    printf("...\n"); // lengkapi di sini. jawaban tidak dibatasi dalam 1 baris
+    printf("Fungi adalah subprogram yang menghasilkan nilai sedankan prosedur tidak\n"); // lengkapi di sini. jawaban tidak dibatasi dalam 1 baris
 
     /* Nomor 3 [5 point] */
     int **val;
     printf("3. Apa tipe data dari deklarasi variabel di atas?");
     printf("Jawaban:\n");
-    printf("...\n");
+    printf("Namanya adalah double pointer dan digunakan untuk point alamat/address sebuah pointer\n");
 
     return 0;
 }
